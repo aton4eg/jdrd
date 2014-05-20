4eb4eb46801bcda8bdb755c7d0cb7587bafac5e6
canayozel@gmail.com <canayozel@gmail.com@cbe63853-923f-ad0f-9ad9-cda128788301>
2014-05-20 21:51:43 +0000

    jar converter has been added

--- trunk/JDRD/src/edu/boun/swe599/jdrd/util/JDRDUtil.java
+++ trunk/JDRD/src/edu/boun/swe599/jdrd/util/JDRDUtil.java
@@ -3,10 +3,13 @@
  */
 package edu.boun.swe599.jdrd.util;
 
+import edu.boun.swe599.jdrd.adpter.JDRDAdapter;
 import java.lang.management.ManagementFactory;
-import java.util.Set;
-import java.util.WeakHashMap;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.util.ASMifier;
+import org.objectweb.asm.util.CheckClassAdapter;
 
 /**
  *
@@ -14,6 +17,32 @@ import org.objectweb.asm.util.ASMifier;
  * @version 1.0 created on May 11, 2014 9:04:35 PM
  */
 public abstract class JDRDUtil {
+
+    public static byte[] convertClassData(byte[] byteCode) {
+        ClassReader classReader = new ClassReader(byteCode);// Class reader to parse the class
+        ClassWriter classWriter = new ClassWriter(classReader, 0);// class reader is passed for performence 
+
+        ClassVisitor classVisitor = new CheckClassAdapter(classWriter); // checks the correctness of the byte code
+//                classVisitor = new TraceClassVisitor(classVisitor, new PrintWriter(System.out)); // prints class
+        classVisitor = new JDRDAdapter(classVisitor);// JDRD instrumentation
+
+        classReader.accept(classVisitor, ClassReader.EXPAND_FRAMES);
+        // Get the result
+        return classWriter.toByteArray();
+    }
+
+    public static long getCurrentProcessId() {
+        String jvm = ManagementFactory.getRuntimeMXBean().getName();
+        return Long.parseLong(jvm.substring(0, jvm.indexOf('@')));
+    }
+
+    public static long getCurrentThreadId() {
+        return Thread.currentThread().getId();
+    }
+
+    public static String getCurrentThreadName() {
+        return Thread.currentThread().getName();
+    }
 
     public static long getIdentity(Object object) {
         return System.identityHashCode(object);
@@ -30,20 +59,8 @@ public abstract class JDRDUtil {
         return false;
     }
 
-    public static long getCurrentThreadId() {
-        return Thread.currentThread().getId();
-    }
-
-    public static String getCurrentThreadName() {
-        return Thread.currentThread().getName();
-    }
-
-    public static long getCurrentProcessId() {
-        String jvm = ManagementFactory.getRuntimeMXBean().getName();
-        return Long.parseLong(jvm.substring(0, jvm.indexOf('@')));
-    }
-
     public static <T> void printASMCode(Class<T> clazz) throws Exception {
         ASMifier.main(new String[]{clazz.getName()});
     }
+
 }
