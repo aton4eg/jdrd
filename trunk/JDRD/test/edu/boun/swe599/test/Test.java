fa242f5461fc0c1d480c2c7bcff4b06bf272e863
canayozel@gmail.com <canayozel@gmail.com@cbe63853-923f-ad0f-9ad9-cda128788301>
2014-05-27 11:18:00 +0000

    fix

--- trunk/JDRD/test/edu/boun/swe599/test/Test.java
+++ trunk/JDRD/test/edu/boun/swe599/test/Test.java
@@ -11,10 +11,57 @@ package edu.boun.swe599.test;
 public class Test {
 
     public static void main(String[] args) {
+        test4();
+    }
+
+    public static void test1() {
+        LoadStoreTest data = new LoadStoreTest();
+        new Thread(new TestThread(data)).start();
+        data.getObject();
+    }
+
+    public static void test1_1() {
+        LoadStoreTest data = new LoadStoreTest();
+        data.getObject();
+        new Thread(new TestThread(data)).start();
+    }
+
+    public static void test1_2() throws InterruptedException {
         LoadStoreTest data = new LoadStoreTest();
         Thread thread1 = new Thread(new TestThread(data));
+        thread1.start();
+        thread1.join();
+        data.getObject();
+    }
 
+    public static void test1_3() {
+        LoadStoreTest data = new LoadStoreTest();
+        Thread thread1 = new Thread(new SyncronizedTestThread(data));
         thread1.start();
         data.getObject();
+    }
+
+    public static void test2() {
+        LoadStoreTest data = new LoadStoreTest();
+        Thread thread1 = new Thread(new TestThread(data));
+        Thread thread2 = new Thread(new TestThread(data));
+        thread1.start();
+        thread2.start();
+    }
+
+    public static void test3() {
+        LoadStoreTest data = new LoadStoreTest();
+        Thread thread1 = new Thread(new SyncronizedTestThread(data));
+        Thread thread2 = new Thread(new SyncronizedTestThread(data));
+        thread1.start();
+        thread2.start();
+    }
+
+    public static void test4() {
+        LoadStoreTest data = new LoadStoreTest();
+        Thread thread1 = new Thread(new TestThread(data));
+        Thread thread2 = new Thread(new SyncronizedTestThread(data));
+        thread1.start();
+        thread2.start();
     }
 }
